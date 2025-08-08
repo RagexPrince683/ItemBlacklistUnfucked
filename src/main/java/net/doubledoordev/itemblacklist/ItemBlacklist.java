978034f54b82de47af773a26ec7b06d65b6374a1
RagexPrince683 <yellowtoad91@gmail.com>
2025-08-07 23:56:38 -0400

    Revert "Added new event that can trigger bans, Any time an inventory is closed banned items will be relocked! Disabled by default, enable via config."
    This reverts commit f30578f6aeb32c7c72834cd35954efe51d50181e.

--- src/main/java/net/doubledoordev/itemblacklist/ItemBlacklist.java
+++ src/main/java/net/doubledoordev/itemblacklist/ItemBlacklist.java
@@ -1,6 +1,11 @@
 package net.doubledoordev.itemblacklist;
 
-import org.apache.logging.log4j.Logger;
+import net.doubledoordev.itemblacklist.client.ClientEventHandlers;
+import net.doubledoordev.itemblacklist.data.GlobalBanList;
+import net.doubledoordev.itemblacklist.util.CommandBlockItem;
+import net.doubledoordev.itemblacklist.util.CommandUnpack;
+import net.doubledoordev.itemblacklist.util.ItemBlacklisted;
+import net.doubledoordev.itemblacklist.util.ServerEventHandlers;
 import net.minecraft.command.CommandHandler;
 import net.minecraft.item.Item;
 import net.minecraftforge.common.MinecraftForge;
@@ -16,13 +21,7 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.common.event.FMLServerStoppedEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
-
-import net.doubledoordev.itemblacklist.client.ClientEventHandlers;
-import net.doubledoordev.itemblacklist.data.GlobalBanList;
-import net.doubledoordev.itemblacklist.util.CommandBlockItem;
-import net.doubledoordev.itemblacklist.util.CommandUnpack;
-import net.doubledoordev.itemblacklist.util.ItemBlacklisted;
-import net.doubledoordev.itemblacklist.util.ServerEventHandlers;
+import org.apache.logging.log4j.Logger;
 
 import static net.doubledoordev.itemblacklist.Helper.MODID;
 import static net.doubledoordev.itemblacklist.Helper.MOD_GUI_FACTORY;
@@ -39,9 +38,8 @@ public class ItemBlacklist
     @Mod.Instance
     public static ItemBlacklist instance;
     public static String message;
-    public static boolean log;
-    public boolean containerban;
 
+    public static boolean log;
     private boolean unpack4all;
     private Configuration configuration;
     private Logger logger;
@@ -79,8 +77,6 @@ public class ItemBlacklist
         message = configuration.getString("message", CATEGORY_GENERAL, "Now is not the time to use that. ~Prof. Oak", "The message you get when using an item that is banned.");
         log = configuration.getBoolean("log", CATEGORY_GENERAL, false, "Log every instance of any banned item used. (SPAM WARNING!)");
         unpack4all = configuration.getBoolean("unpack4all", CATEGORY_GENERAL, true, "Let everyone unpack items by using the 'unpack' command. So items can be used in crafting.");
-        containerban = configuration.getBoolean("containerban", CATEGORY_GENERAL, false, "Got a really stubborn item to ban? Enable this! Items will be banned whenever an inventory closes.");
-
 
         if (configuration.hasChanged()) configuration.save();
 
